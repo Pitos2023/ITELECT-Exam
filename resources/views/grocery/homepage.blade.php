4bbe776953364e49f2b1fabeab09c1d67196d919
Pitos2023 <chrismanuelpitos.202200645@gmail.com>
2024-09-10 10:02:18 +0800

    First commit

--- resources/views/grocery/homepage.blade.php
+++ resources/views/grocery/homepage.blade.php
@@ -6,9 +6,22 @@
 </head>
 <body>
     <div class ="navbar">
-        <a href="/home">Shop</a>
-        <a href="/cart">Cart</a>
-        <a href="/list">List</a>
+            <a href="/home">Shop</a>
+            <a href="/cart">Cart</a>
+            <a href="/list">List</a>
+    </div>
+
+    <div class ="grocery-list">
+        <div class ="item item1">1</div>
+        <div class ="item item2">2</div>
+        <div class ="item item3">3</div>
+        <div class ="item item4">4</div>
+        <div class ="item item5">5</div>
+        <div class ="item item6">6</div>
+        <div class ="item item7">7</div>
+        <div class ="item item8">8</div>
+        <div class ="item item9">9</div>
+        <div class ="item item10">10</div>
     </div>
 </body>
 </html>
